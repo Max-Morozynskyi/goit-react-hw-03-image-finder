9393419266d1d4f007c2dd8977c009de98330d66
Max-Morozynskyi <m0r0zin@ukr.net>
2023-02-06 18:56:21 +0200

    sarchbar added

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,32 @@
-export const App = () => {
-  return <div>React homework template</div>;
-};
+import React, { Component } from 'react';
+import { AppDiv } from './App.styled';
+import { Modal } from './Modal/Modal';
+import { Searchbar } from './Searchbar/Searchbar';
+
+export class App extends Component {
+  state = {
+    isModalOpen: false,
+    searchValue: '',
+  };
+
+  toggleModal = () => {
+    this.setState(({ isModalOpen }) => ({ isModalOpen: !isModalOpen }));
+  };
+
+  handleSearchState = searchValue => {
+    // this.setState({ searchValue });
+    console.log('searchValue', searchValue);
+  };
+
+  render() {
+    const { isModalOpen } = this.state;
+
+    return (
+      <AppDiv>
+        <Searchbar qwert={this.handleSearchState} />
+        {isModalOpen && <Modal onClose={this.toggleModal}>popup</Modal>}
+        React homework template
+      </AppDiv>
+    );
+  }
+}
